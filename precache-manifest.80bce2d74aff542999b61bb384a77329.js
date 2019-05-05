4c218bf8456032a7a52050633a37056fd315be80
mtthai <marvinthai@yahoo.com>
2019-05-05 01:28:05 -0500

    Updates

---
+++ precache-manifest.80bce2d74aff542999b61bb384a77329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "361bf24f63197a419d3a",
+    "url": "/nba-foul-viewer/static/js/runtime~main.361bf24f.js"
+  },
+  {
+    "revision": "07b2f84d7377b22680a5",
+    "url": "/nba-foul-viewer/static/js/main.07b2f84d.chunk.js"
+  },
+  {
+    "revision": "11ff500840e630d2a02b",
+    "url": "/nba-foul-viewer/static/js/2.11ff5008.chunk.js"
+  },
+  {
+    "revision": "07b2f84d7377b22680a5",
+    "url": "/nba-foul-viewer/static/css/main.5b720583.chunk.css"
+  },
+  {
+    "revision": "ff20c676d4840a00e0462ee6c126e11e",
+    "url": "/nba-foul-viewer/index.html"
+  }
+];
